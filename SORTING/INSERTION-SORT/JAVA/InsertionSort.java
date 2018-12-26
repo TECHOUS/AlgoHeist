c747c1858afb282d2d06010324978894cf33ce1e
GauravWalia19 <gauravwalia019@gmail.com>
2018-12-26 14:56:52 +0530

    added insertion sort in java

---
+++ SORTING/INSERTION-SORT/JAVA/InsertionSort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class InsertionSort
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n; //size of the array
+        System.out.println("Enter the size of the array: ");
+        n = in.nextInt();
+        int[] arr = new int[n]; //declared array
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = in.nextInt();
+        }
+        insertion(arr);
+        System.out.println("The sorted array: ");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        in.close();
+    }
+
+    public static void insertion(int[] array)
+    {
+        for(int i=1;i<array.length;i++)
+        {
+            int raw = array[i]; //created a raw element which we insert
+            int j;
+            for(j=i;j>0 && array[j-1] < raw;j--)
+            {
+                //shifting from left to right
+                array[j] = array[j-1];
+            }
+            array[j] = raw;
+        }
+    }
+}
---
+++ SORTING/INSERTION-SORT/JAVA/insertionSort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class insertionSort
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        int N; //size of the array
+        System.out.println("Enter the size of the array");
+        N = input.nextInt();
+        int[] arr = new int[N]; //declaring array
+        for(int i=0;i<N;i++)
+        {
+            arr[i] = input.nextInt();
+        }
+        insertionsort(arr);
+        //printing the array
+        System.out.println("The sorted array: ");
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        input.close();
+    }
+
+    public static void insertionsort(int[] array)
+    {
+        for(int i=1;i<array.length;i++)
+        {
+            int raw = array[i];
+            int j;
+            for(j=i;j>0 && array[j-1] > raw;j--)
+            {
+                array[j] = array[j-1]; //shifting
+            }
+            array[j] = raw;
+        }
+    }
+}
