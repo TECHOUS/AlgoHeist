082a22b4bb6a8c0aacd6af2bcac49541b58ff020
ashwek <ashwekswamy@protonmail.com>
2019-01-14 19:54:55 +0530

    Create Pallindrome.java

---
+++ STRINGS/Pallindrome/JAVA/Pallindrome.java
@@ -0,0 +1,32 @@
+class Pallindrome{
+
+    public static boolean isPallindrome(String str){
+
+        int len = str.length();
+
+        for(int i=0; i<(len/2); i++){
+            // comparing ith character with "Length-i-1"th character
+            if( str.charAt(i) != str.charAt(len-i-1) ){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public static void main(String args[]){
+
+        // Input is taken through command-line argument
+
+        if( args.length == 0 ){
+            System.out.println("No string provided in command-line argument");
+        }
+        else{
+            System.out.print(args[0] + " is ");
+            if( ! isPallindrome(args[0]) ){
+                System.out.print("not ");
+            }
+            System.out.println("Pallindrome");
+        }
+    }
+}
