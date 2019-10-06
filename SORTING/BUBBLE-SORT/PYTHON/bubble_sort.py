1cc6c84c6c8e377bb707406d6957a026ed70c5ab
Rituparna <ri2parna@outlook.com>
2019-10-07 01:25:26 +0530

    added bubble sort in python3

---
+++ SORTING/BUBBLE-SORT/PYTHON/bubble_sort.py
@@ -0,0 +1,20 @@
+# Bubble sort ,Python
+
+def bubbleSort(arr):
+	n = len(arr)
+	# iterate through all the elements of the array.
+	for i in range(n):
+		# The last i elements are already in place.
+		for j in range(0, n-i-1):
+			# traverse the array from 0 to n-i-1
+			# Swap if the element found is greater
+			# than the next element
+			if arr[j] > arr[j+1] :
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+# driver Fucntion
+arr = [14, 38, 22, 12, 92, 61, 98]
+bubbleSort(arr)
+print ("The sorted Sorted array is:", arr)
+#for i in range(len(arr)):
+#:	print ("%d" %arr[i]), 
+
