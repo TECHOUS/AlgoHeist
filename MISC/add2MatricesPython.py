8c5e412a28e827309f993173f45373d43cc20cc3
chinifoxtrot <56346300+chinifoxtrot@users.noreply.github.com>
2019-10-10 00:59:18 +0530

    add 2 matrices using python

---
+++ MISC/add2MatricesPython.py
@@ -0,0 +1,15 @@
+# Program to add two matrices
+# using list comprehension
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[X[i][j] + Y[i][j]  for j in range(len(X[0]))] for i in range(len(X))]
+
+for r in result:
+   print(r)
