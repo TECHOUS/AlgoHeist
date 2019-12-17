6c289052604f6a16038467aac18ff71dce1846ef
GauravWalia19 <gauravwalia019@gmail.com>
2019-12-18 00:16:43 +0530

    fixed ENHANCEMENT Issue #75

--- SEARCHING/LINEAR-SEARCH/Python/linearSearch.py
+++ SEARCHING/LINEAR-SEARCH/Python/linearSearch.py
@@ -1,17 +1,19 @@
-def linearSearch(arr, find):
+# this function contains the main logic for linear search
+def linearSearch(array, find):
 
-    for i in range(len(arr)):
-        if find == arr[i] :
+    for i in range(len(array)):
+        if find == array[i] :
             return i
 
     return -1
 
-arr = list(map(int, input("Enter values in list (space separated) = ").split()))
+
+array = list(map(int, input("Enter values in list (space separated) = ").split()))
 find = int(input("Enter value to find = "))
 
-loc = linearSearch(arr, find)
+index = linearSearch(array, find)
 
-if loc == -1 :
+if index == -1 :
     print("value not found")
 else:
-    print("value found at index", loc)
+    print("value found at index", index)
