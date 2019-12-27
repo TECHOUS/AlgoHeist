2891b4a42911293eeb16024a1b8ee3ec1d8e2ae8
pankhuri-52 <pankhuritrikha@gmail.com>
2019-12-28 00:03:30 +0530

    Fixes issue #73

--- STRINGS/REVERSE-A-STRING/C/reverse-a-string.c
+++ STRINGS/REVERSE-A-STRING/C/reverse-a-string.c
@@ -1,12 +1,9 @@
-//reverse a string
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-int main()
-{
-    char str[1000];
-    fgets(str,1000,stdin);
-    int len = strlen(str);
+
+//method to reverse a string in c language
+void string_reversed(char str[1000],int len){
     for(int i=0;i<len/2;i++)
     {
         char temp=str[i];
@@ -14,4 +11,12 @@ int main()
         str[len-i-1]=temp;
     }
     puts(str);
+}
+int main()
+{
+    char str[1000];
+    fgets(str,1000,stdin);
+    int length = strlen(str);
+    string_reversed(str,length); //passed string and length of string to this function
+    return 0;
 }
