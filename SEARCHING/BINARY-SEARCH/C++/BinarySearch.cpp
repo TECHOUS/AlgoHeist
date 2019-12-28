4e92d605ff0453fcff47624fd55621ecbe340615
Nitin Gupta <gupta7nitin@gmail.com>
2019-12-28 08:26:32 +0530

    Fixes Issue #77

--- SEARCHING/BINARY-SEARCH/C++/BinarySearch.cpp
+++ SEARCHING/BINARY-SEARCH/C++/BinarySearch.cpp
@@ -1,55 +1,40 @@
-#include<iostream>
+// C++ program to implement recursive Binary Search
+#include <iostream>
 using namespace std;
+
+// A iterative binary search function. It returns
+// location of x in given array arr[l..r] if present,
+// otherwise -1
+int binarySearch(int arr[], int l, int r, int x)
+{
+	while (l <= r) {
+		int m = l + (r - l) / 2;
+
+		// Check if x is present at mid
+		if (arr[m] == x)
+			return m;
+
+		// If x greater, ignore left half
+		if (arr[m] < x)
+			l = m + 1;
+
+		// If x is smaller, ignore right half
+		else
+			r = m - 1;
+	}
+
+	// if we reach here, then element was
+	// not present
+	return -1;
+}
+
 int main()
 {
-    int n,a[100],x,mid=0,s=0,e=0;
-    cout<<"Enter size of array: ";
-    cin>>n;
-    cout<<"Enter elements of array: \n";
-    for(int i=0; i<n; i++)
-    {
-        cin>>a[i];
-    }
-    for(int i=0; i<n; i++)
-    {
-        for(int j=0; j<n-i; j++)
-        {
-            if(a[j]>a[j+1])
-            {
-                int temp = a[j];
-                a[j] = a[j+1];
-                a[j+1] = temp;
-            }
-        }
-    }
-    cout<<"Sorted array: "<<endl;
-    for(int i=0; i<n; i++)
-    {
-        cout<<a[i]<<" "<<endl;
-    }
-    e = n-1;
-    cout<<"Enter a no to search: ";
-    cin>>x;
-    mid = (s+e)/2;
-    while(s<e && a[mid]!=x)
-    {
-        if(x>a[mid])
-        {
-            s = mid+1;
-        }
-        else
-        {
-            e = mid-1;
-        }
-        mid = (s+e)/2;
-    }
-    if(a[mid] == x)
-    {
-        cout<<"Element "<<a[mid]<<" found at "<<mid+1<<" position"<<endl;
-    }
-    else
-    {
-        cout<<"Element not found"<<endl;
-    }
-    return 0;
+	int arr[] = { 2, 3, 4, 10, 40 };
+	int x = 10;
+	int n = sizeof(arr) / sizeof(arr[0]);
+	int result = binarySearch(arr, 0, n - 1, x);
+	(result == -1) ? cout << "Element is not present in array"
+				: cout << "Element is present at index " << result;
+	return 0;
 }
