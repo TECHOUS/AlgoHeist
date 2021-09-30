2cf692d8a97e83905548f2538ad6f2a2a22a3f7d
Ishita <ishita0jha29@gmail.com>
2021-10-01 00:38:33 +0530

    Updated a short Code for Binary search in Java
    I have updated a short code of binary search in Java, incase you find it helpful please merge it :)

--- searching/binary_search/binarySearch.java
+++ searching/binary_search/binarySearch.java
@@ -1,55 +1,23 @@
-import java.util.Scanner;
+class Solution {
+    public int search(int[] arr, int target) {
+        int s = 0;
+        int e = arr.length -1;
+        while(s <= e)
+        {
+            int mid = s + (e - s) / 2; 
 
-class BinarySearch{
-
-    /**
-     * this function contains the code for iterative binary search
-     * @param array
-     * @param find
-     * @return int
-     **/
-    public static int binarySearch(int array[], int find){
-        int startIndex = 0, endIndex = array.length, midIndex;
-
-        while( startIndex < endIndex ){
-            midIndex = (startIndex + endIndex) / 2;
-            if( array[midIndex] == find ){
-                return midIndex;
+            if(target<arr[mid])
+            {
+                e = mid - 1;
             }
-            else if( find < array[midIndex] ){
-                endIndex = midIndex;
+            else if(target>arr[mid])
+            {
+                s = mid + 1;
             }
             else{
-                startIndex = midIndex+1;
+                return mid;
             }
         }
-        return -1;
-    }
-
-    public static void main(String args[]){
-        int size, find, loc;
-        Scanner in = new Scanner(System.in);
-
-        System.out.print("Enter the size of array = ");
-        size = in.nextInt();
-
-        int array[] = new int[size];
-        System.out.println("Enter " + size + " sorted elements in array :");
-        for(int i=0; i<size; i++){
-            array[i] = in.nextInt();
-        }
-
-        System.out.print("Enter value to find = ");
-        find = in.nextInt();
-
-        loc = binarySearch(array, find);
-
-        if( loc == -1 ){
-            System.out.println("value Not found in array");
-        }
-        else{
-            System.out.println("value found at index " + loc);
-        }
-        in.close();
+       return -1;
     }
 }
