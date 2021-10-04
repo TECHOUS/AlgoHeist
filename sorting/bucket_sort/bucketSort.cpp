f94bab2dcf94c860b4fbac9b5d30e76fba0f1d16
AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>
2021-10-04 22:34:12 +0530

    Updated the required changes w/signoff
    Signed-off-by: AnshumanDhiman <67755381+AnshumanDhiman@users.noreply.github.com>

--- sorting/bucket_sort/bucketSort.cpp
+++ sorting/bucket_sort/bucketSort.cpp
@@ -2,21 +2,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- 
 
 void bucketSort(float arr[], int x)
 {
-     
-
     vector<float> b[x];
- 
- 
     for (int i = 0; i < x; i++) {
         int bi = x * arr[i];
         b[bi].push_back(arr[i]);
     }
- 
-
     for (int i = 0; i < x; i++)
         sort(b[i].begin(), b[i].end());
  
@@ -27,12 +20,11 @@ void bucketSort(float arr[], int x)
             arr[index++] = b[i][j];
 }
  
-
 int main()
 {
     float arr[]
         = { 0.845, 0.455, 0.601, 0.1124, 0.695, 0.3224 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = sizeof(arr) / sizeof(arr[0]);
     bucketSort(arr, x);
  
     cout << "Sorted array \n";
